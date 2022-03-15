5fd5b9934c1c882a59bfed6fe17f9d37c5f6da48
Zhixin He <zhixin.he@hotmail.com>
2022-03-15 11:23:36 -0700

    add a print prime number program

---
+++ wk-2/PrintPrimeNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Please enter a number <larger than 2>: ");
+int numUpperRange = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The prime numbers not bigger than {numUpperRange} listed below:");
+
+int i = 0;
+int j = 0;
+int n = 0;
+const int NLINE = 10;
+for (i = 2; i <= numUpperRange; i++)
+{
+    for (j = 2; j <= i/j ; j++)
+    {
+        if (i%j == 0) break;
+    }
+    if (j > i/j) 
+    {
+        if (n%NLINE == 0) Console.WriteLine();
+        Console.Write(format:"{0,10}", arg0:i);
+        n++;
+    }
+}
