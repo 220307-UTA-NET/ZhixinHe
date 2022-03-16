b9c44e9d29c9911439a7a194b9db2932a1dea0a2
Zhixin He <zhixin.he@hotmail.com>
2022-03-16 11:32:44 -0700

    complete the wk-2 string challenge/test

---
+++ wk-2/CSharpChallenges/1_StringManipulationChallenge - StudentCopy/StringManipulationChallenge/StringManipulationChallenge.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace StringManipulationChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputString; //this will hold your users message
+            int elementNum;         //this will hold the element number within the messsage that your user indicates
+            char char1;             //this will hold the char value that your user wants to search for in the message.
+            string fName;           //this will hold the users first name
+            string lName;           //this will hold the users last name
+            string userFullName;    //this will hold the users full name;
+            
+            //
+            //
+            //implement the required code here and within the methods below.
+            //
+            //
+
+
+        }
+
+        // This method has one string parameter. 
+        // It will:
+        // 1) change the string to all upper case, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringToUpper(string x){
+            return x.ToUpper();
+        }
+
+        // This method has one string parameter. 
+        // It will:
+        // 1) change the string to all lower case, 
+        // 2) print the result to the console and 
+        // 3) return the new string.        
+        public static string StringToLower(string x){
+            return x.ToLower();
+        }
+        
+        // This method has one string parameter. 
+        // It will:
+        // 1) trim the whitespace from before and after the string, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringTrim(string x){
+            int startPos = 0;
+            int endPos = x.Length;
+            for ( startPos = 0; startPos < endPos; startPos++ )
+            {
+                if ( x[startPos] != ' ' ) break;
+            }
+            for ( endPos = x.Length - 1; endPos > startPos; endPos-- )
+            {
+                if ( x[endPos] != ' ' ) break;
+            }
+            return x.Substring(startPos, endPos - startPos + 1);
+        }
+        
+        // This method has two parameters, one string and one integer. 
+        // It will:
+        // 1) get the substring based on the integer received, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringSubstring(string x, int elementNum){
+            return x.Substring(elementNum);
+        }
+
+        // This method has two parameters, one string and one char.
+        // It will:
+        // 1) search the string parameter for the char parameter
+        // 2) return the index of the char.
+        public static int SearchChar(string userInputString, char x){
+            int pos = -1;
+            for ( int i = 0; i < userInputString.Length; i++)
+            {
+                if ( x == userInputString[i] ) 
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            return pos;
+        }
+
+        // This method has two string parameters.
+        // It will:
+        // 1) concatenate the two strings with a space between them.
+        // 2) return the new string.
+        public static string ConcatNames(string fName, string lName){
+            return fName + " " + lName;
+        }
+
+
+
+    }//end of program
+}
