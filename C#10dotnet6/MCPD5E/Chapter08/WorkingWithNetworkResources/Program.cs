452731904db541ee6c4e8799abf5673e8e9dcf11
Zhixin He <zhixin.he@hotmail.com>
2022-03-19 22:03:47 -0700

    completing Chapter08

---
+++ C#10dotnet6/MCPD5E/Chapter08/WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using static System.Console;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+      
+Write("Enter a valid web address: ");
+string? url = ReadLine();
+
+if (string.IsNullOrWhiteSpace(url))
+{
+    url = "http://stackoverflow.com/search?q=securestring";
+}
+
+Uri uri = new(url);
+
+IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+WriteLine($"{entry.HostName} has the following IP addresses:");
+foreach (IPAddress address in entry.AddressList)
+{
+    WriteLine($"    {address} ({address.AddressFamily})");
+}
+
+WriteLine($"URL: {url}");
+WriteLine($"Schema: {uri.Scheme}");
+WriteLine($"Port: {uri.Port}");
+WriteLine($"Host: {uri.Host}");
+WriteLine($"Path: {uri.AbsolutePath}");
+WriteLine($"Query: {uri.Query}");
+        }
+    }
+}
---
+++ C#10dotnet6/MCPD5E/Chapter08/WorkingWithReflection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using static System.Console;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Assembly metadata:");
+            Assembly? assembly = Assembly.GetEntryAssembly();
+            if (assembly is null)
+            {
+                WriteLine("Failed to get entry assembly.");
+                return;
+            }
+
+            WriteLine($"    Full name: {assembly.FullName}");
+            WriteLine($"    Location: {assembly.Location}");
+
+            IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+            WriteLine($"    Assembly-level attributes:");
+            foreach (Attribute a in attributes)
+            {
+                WriteLine($"    {a.GetType()}");
+            }
+        }
+    }
+}
